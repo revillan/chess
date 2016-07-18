c2a3dbdb90e7c80de48aeb12b6bc0633406ff74e
Reilly Villanueva <reillyvillanueva@gmail.com>
2016-07-18 13:50:47 -0700

    board class initialized

---
+++ board.rb
@@ -0,0 +1,29 @@
+class Board
+
+  def initialize
+    @grid = Array.new(8) {Array.new(8)}
+  end
+
+  def [](pos)
+    x, y = pos
+    @grid[x][y]
+  end
+
+  def []=(pos, value)
+    x, y = pos
+    @grid[x][y] = value
+  end
+
+  def move(start, end_pos)
+    self[end_pos] = self[start]
+    self[start] = nil
+    begin
+      if self[start].nil? # || self[end_pos]
+        raise ArgumentError.new "Start position is empty"
+      end
+    rescue ArgumentError => e
+      puts e.message
+    end
+  end
+
+end
